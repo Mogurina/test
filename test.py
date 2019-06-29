7c890ba8e0cbb60d8c91dfa742dd4c5212d56b85
mogurina <mogumogutyannneru@icloud.com>
2019-06-29 19:32:54 +0900

    change code

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import cv2
 imgname = input("::")
 img = cv2.imread(imgname,1)
-img = cv2.Canny(img,100,200)
-cv2.imshow(imgname,img)
-cv2.waitKey()
+if img is None:
+    print("can not open image!")
+else:
+    #img = cv2.Canny(img,100,200)
+    cv2.imshow(imgname,img)
+    cv2.waitKey()
+    
